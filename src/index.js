18591644bbfd274c2baf411e354bd0e9c387f37a
Madala Sai Vishnu <sai.vishnu@altudo.co>
2025-08-04 15:32:09 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 //require('dotenv').config({path: './env'})
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './env'
 })
 
 connectDB()
-
-
+.then(() => {
+    app.on('error', (error) => {
+        console.log("ERROR: ", error);
+        throw error;
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port: ${process.env.PORT}`);   
+    })
+})
+.catch((error) => {
+    console.log("MONGO DB connection failed !!", error);
+})
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,20 @@
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((error) => next(error))
+    }
+}
+
+
+export {asyncHandler}
+
+
+// const asyncHandler = (fn) => async (res, req, next) => {
+//     try {
+//         await fn(res, req, next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
